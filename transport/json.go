589852f645c132337127ce9b1bf581e9569601ff
Mitchell Cooper <githubspam@rlygd.net>
2017-02-10 21:20:06 -0500

    check for error

--- transport/json.go
+++ transport/json.go
@@ -61,12 +61,21 @@ func (jsonTr *jsonTransport) readLoop() {
 func (jsonTr *jsonTransport) mainLoop() {
 	for {
 		select {
+
+        // read error
+        case err := <-jsonTr.err:
+            log.Println("error reading!", err)
+            go jsonTr.readLoop()
+
+        // outgoing messages
 		case msg := <-jsonTr.writeMessages:
 			log.Println("found a message to write:", msg)
 			data := append(msg.ToJson(), '\n')
 			if _, err := jsonTr.writer.Write(data); err != nil {
 				log.Println("error writing!", err)
 			}
+
+        // incoming json data
 		case json := <-jsonTr.incoming:
 			log.Println("found some data to handle:", string(json))
 			msg, err := wikiclient.MessageFromJson(json)
