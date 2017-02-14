8fb4338b8e1a000183cfa65b8c46f693599114c2
Mitchell Cooper <githubspam@rlygd.net>
2017-02-13 21:39:59 -0500

    add page request

--- http-handlers.go
+++ http-handlers.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/cooper/quiki/wikiclient"
 	"log"
 	"net/http"
@@ -9,6 +10,14 @@ import (
 
 func handlePage(c wikiclient.Client, relPath string, w http.ResponseWriter, r *http.Request) {
 	log.Println(relPath, c, r)
+	res, err := c.Request(wikiclient.NewMessage("page", map[string]interface{}{
+		"name": relPath,
+	}))
+	if err != nil {
+		fmt.Fprint(w, err)
+		return
+	}
+	fmt.Fprint(w, res)
 }
 
 func handleImage(c wikiclient.Client, relPath string, w http.ResponseWriter, r *http.Request) {
