5f7288292a7f76cc2d2c8cef3d27c1513650708b
Mitchell Cooper <githubspam@rlygd.net>
2017-03-18 22:11:36 -0400

    move up

--- wiki.go
+++ wiki.go
@@ -147,6 +147,8 @@ func setupWiki(wiki wikiInfo) error {
 			root = wikiRoot + root
 		}
 
+		log.Printf("N: %s\nT: %s\nH: %s\nR: %s\n", wiki.name, rootType, wiki.host, root)
+
 		root += "/"
 
 		// add the real handler
@@ -170,7 +172,6 @@ func setupWiki(wiki wikiInfo) error {
 
 			handler(wiki, relPath, w, r)
 		})
-		log.Printf("N: %s\nT: %s\nH: %s\nR: %s\n", wiki.name, rootType, wiki.host, root)
 		log.Printf("[%s] registered %s root: %s", wiki.name, rootType, wiki.host+root)
 	}
 
