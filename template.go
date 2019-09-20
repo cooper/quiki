8bc779d838d4eb8ecba435cb5ca7db90fb79b30d
Mitchell Cooper <githubspam@rlygd.net>
2019-09-19 21:00:22 -0700

    no more retinajs

--- template.go
+++ template.go
@@ -196,7 +196,6 @@ func (p wikiPage) VisibleTitle() string {
 func (p wikiPage) Scripts() []string {
 	return []string{
 		"/static/mootools.min.js",
-		"/static/retina.min.js",
 		"/static/wiki.js",
 		"https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js",
 	}
