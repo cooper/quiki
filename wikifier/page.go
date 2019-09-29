338c780bc0ef4186cc485131d08891eb45c7f503
Mitchell Cooper <githubspam@rlygd.net>
2019-09-28 21:26:17 -0700

    Redirect, IsSymlink, Created

--- wikifier/page.go
+++ wikifier/page.go
@@ -6,7 +6,11 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
+
+	httpdate "github.com/Songmu/go-httpdate"
 )
 
 // Page represents a single page or article, generally associated with a .page file.
@@ -96,21 +100,6 @@ func (p *Page) Exists() bool {
 	return err == nil
 }
 
-// # page filename, with or without extension.
-// # this DOES take symbolic links into account.
-// # this DOES include the page prefix, if applicable.
-// sub name {
-//     my $page = shift;
-//     return $page->{abs_name}
-//         if length $page->{abs_name};
-//     return $page->{cached_props}{name} //= do {
-//         my $dir  = $page->opt('dir.page');
-//         my $path = $page->path;
-//         (my $name = $path) =~ s/^\Q$dir\E(\/?)//;
-//         index($path, $dir) ? $page->rel_name : $name;
-//     };
-// }
-
 // Name returns the resolved page name, with or without extension.
 //
 // This does NOT take symbolic links into account.
@@ -180,6 +169,143 @@ func (p *Page) RelNameNE() string {
 func (p *Page) RelPath() string {
 	return p.FilePath
 }
+
+// Redirect returns the location to which the page redirects, if any.
+// This may be a relative or absolute URL, suitable for use in a Location header.
+func (p *Page) Redirect() string {
+
+	// symbolic link redirect
+	if p.IsSymlink() {
+		return p.Opt.Root.Page + "/" + p.NameNE()
+	}
+
+	// @page.redirect
+	if link, err := p.GetStr("page.redirect"); err != nil {
+		// FIXME: is there anyway to produce a warning for wrong variable type?
+	} else if ok, _, target, _, _, _ := parseLink(link); ok {
+		return target
+	}
+
+	return ""
+}
+
+// IsSymlink returns true if the page is a symbolic link to another file within
+// the page directory. If it is symlinked to somewhere outside the page directory,
+// it is treated as a normal page rather than a redirect.
+func (p *Page) IsSymlink() bool {
+	if !strings.HasPrefix(p.Prefix(), p.Opt.Dir.Page) {
+		return false
+	}
+	fi, _ := os.Lstat(p.RelPath())
+	return fi.Mode()&os.ModeSymlink != 0
+}
+
+// Created returns the page creation time.
+func (p *Page) Created() time.Time {
+	var t time.Time
+	// FIXME: maybe produce a warning if this is not in the right format
+	created, _ := p.GetStr("page.created")
+	if created == "" {
+		return t
+	}
+	if unix, err := strconv.ParseInt(created, 10, 0); err != nil {
+		return time.Unix(unix, 0)
+	}
+	t, _ = httpdate.Str2Time(created, time.UTC)
+	return t
+}
+
+// # page modification time from stat()
+// sub modified {
+//     my $page = shift;
+//     return (stat $page->path)[9];
+// }
+
+// # absolute path to cache file
+// sub cache_path {
+//     my $page = shift;
+//     return abs_path($page->{cache_path})
+//         if length $page->{cache_path};
+//     make_dir($page->opt('dir.cache').'/page', $page->name);
+//     return $page->{abs_cache_path}
+//         if length $page->{abs_cache_path};
+//     return $page->{cached_props}{cache} //= abs_path(
+//         $page->opt('dir.cache').'/page/'.$page->name.'.cache'
+//     );
+// }
+
+// # cache file modification time from stat()
+// sub cache_modified {
+//     my $page = shift;
+//     return (stat $page->cache_path)[9];
+// }
+
+// # absolute path to search text file
+// sub search_path {
+//     my $page = shift;
+//     return abs_path($page->{search_path})
+//         if length $page->{search_path};
+//     make_dir($page->opt('dir.cache').'/page', $page->name);
+//     return $page->{abs_search_path}
+//         if length $page->{abs_search_path};
+//     return $page->{cached_props}{search} //= abs_path(
+//         $page->opt('dir.cache').'/page/'.$page->name.'.txt'
+//     );
+// }
+
+// # page info to be used in results, stored in cats/cache files
+// sub page_info {
+//     my $page = shift;
+//     return filter_nonempty {
+//         mod_unix    => $page->modified,
+//         created     => $page->created,
+//         draft       => $page->draft,
+//         generated   => $page->generated,
+//         redirect    => $page->redirect,
+//         fmt_title   => $page->fmt_title,
+//         title       => $page->title,
+//         author      => $page->author
+//     };
+// }
+
+// sub _bool ($) { shift() ? \1 : undef }
+
+// # page draft from @page.draft
+// sub draft {
+//     my $page = shift;
+//     return _bool $page->get('page.draft');
+// }
+
+// # page generated from @page.generated
+// sub generated {
+//     my $page = shift;
+//     return _bool $page->get('page.generated');
+// }
+
+// # page author from @page.author
+// sub author {
+//     my $page = shift;
+//     return no_length_undef trim $page->get('page.author');
+// }
+
+// # formatted title from @page.title
+// sub fmt_title {
+//     my $page = shift;
+//     return no_length_undef trim $page->get('page.title');
+// }
+
+// # tag-stripped version of page title
+// sub title {
+//     my $page = shift;
+//     my $title = $page->fmt_title;
+//     return length $title ? $stripper->parse($title) : undef;
+// }
+
+// # title if available; otherwise filename
+// sub title_or_name {
+//     my $page = shift;
+//     return $page->title // $page->name;
+// }
 
 // resets the parser
 func (p *Page) resetParseState() {
