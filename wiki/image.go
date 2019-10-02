6d3740ecdcaa52b67f5645558706acac79958103
Mitchell Cooper <githubspam@rlygd.net>
2019-10-01 21:48:08 -0700

    symlink immediately after generating it instead

--- wiki/image.go
+++ wiki/image.go
@@ -212,15 +212,14 @@ func (w *Wiki) DisplaySizedImageGenerate(img SizedImage, generateOK bool) interf
 
 	// find missing dimension
 	// note: we haven't checked if both are 0 yet, but this will return 0, 0 in that case
-	oldName := img.ScaleName()
+	oldName := img.FullName()
 	img.Width, img.Height = calculateImageDimensions(bigW, bigH, img.Width, img.Height)
 
 	// check if the name has changed after this adjustment.
 	// if so, redirect
-	scaleName := img.ScaleName()
 	fullName := img.FullName()
-	if scaleName != oldName || img.zeroByZero {
-		return DisplayRedirect{Redirect: w.Opt.Root.Image + "/" + scaleName}
+	if fullName != oldName || img.zeroByZero {
+		return DisplayRedirect{Redirect: w.Opt.Root.Image + "/" + fullName}
 	}
 
 	// image name and full path
@@ -298,9 +297,6 @@ func (w *Wiki) DisplaySizedImageGenerate(img SizedImage, generateOK bool) interf
 			r.ModifiedHTTP = httpdate.Time2Str(mod)
 			r.Length = cacheFi.Size()
 
-			// symlink if necessary
-			w.symlinkScaledImage(img, scaleName)
-
 			return r
 		}
 	}
@@ -327,6 +323,7 @@ func (w *Wiki) DisplaySizedImageGenerate(img SizedImage, generateOK bool) interf
 		return dispErr
 	}
 
+	w.symlinkScaledImage(img, fullName)
 	return r
 }
 
