29278c5992ff586384891a4d5115e5ba1b0f2725
Mitchell Cooper <githubspam@rlygd.net>
2020-02-07 10:50:13 -0500

    authenticator read/write

--- authenticator/authenticator.go
+++ authenticator/authenticator.go
@@ -1,11 +1,58 @@
 // Package authenticator provides server and site authentication services.
 package authenticator
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
 // Authenticator represents a quiki server or site authentication service.
 type Authenticator struct {
+	path string
 }
 
 // Open reads a user data file and returns an Authenticator for it.
+// If the path does not exist, a new data file is created.
 func Open(path string) (*Authenticator, error) {
-	return nil, nil
+	auth := &Authenticator{path: path}
+
+	// attempt to read the file
+	jsonData, err := ioutil.ReadFile(path)
+
+	// it exists; try to unmarshal it
+	if err == nil {
+		err = json.Unmarshal(jsonData, auth)
+
+		// JSON data is no good?
+		// I mean, we can't just purge it because the data would be lost.
+		// guess it needs some manual intervention...
+		if err != nil {
+			return nil, err
+		}
+
+		// all good
+		return auth, nil
+	}
+
+	// hmm, a ReadFile error occurred OTHER THAN file does not exist
+	if !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	// create a new one
+	return auth, auth.write()
+}
+
+// Write overwrites the data file with the current contents of the Authenticator.
+func (auth *Authenticator) write() error {
+
+	// encode as JSON
+	jsonData, err := json.Marshal(auth)
+	if err != nil {
+		return err
+	}
+
+	// write
+	return ioutil.WriteFile(auth.path, jsonData, 0666)
 }
