a9a594694f94849d4ab03f52c04011e6ee9c8752
Mitchell Cooper <githubspam@rlygd.net>
2020-02-19 14:33:56 -0500

    begin work on git: IsRepo, InitRepo

--- webserver/wiki.go
+++ webserver/wiki.go
@@ -88,6 +88,9 @@ func initWikis() error {
 		// create wiki info for webserver
 		wi := &WikiInfo{Wiki: w, Host: wikiHost, Name: wikiName}
 
+		// initialize git repsitory
+		w.InitRepo()
+
 		// pregenerate
 		w.Pregenerate()
 
--- wiki/revision.go
+++ wiki/revision.go
@@ -1,1 +1,25 @@
 package wiki
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/ldez/go-git-cmd-wrapper/git"
+	ginit "github.com/ldez/go-git-cmd-wrapper/init"
+)
+
+// IsRepo returns true if the wiki directory is versioned by git.
+func (w *Wiki) IsRepo() bool {
+	_, err := os.Stat(filepath.Join(w.Opt.Dir.Wiki, ".git"))
+	return err == nil
+}
+
+// InitRepo initializes a git repository for the wiki.
+// If the wiki is already versioned by git, no error is produced.
+func (w *Wiki) InitRepo() error {
+	if w.IsRepo() {
+		return nil
+	}
+	_, err := git.Init(ginit.Quiet, ginit.Directory(w.Opt.Dir.Wiki))
+	return err
+}
