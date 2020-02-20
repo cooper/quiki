069d61a55fd0e4f07484242b8e578fc0378c5704
Mitchell Cooper <githubspam@rlygd.net>
2020-02-19 20:52:12 -0800

    count all secs, not just top-level

--- wikifier/block-toc.go
+++ wikifier/block-toc.go
@@ -1,33 +1,33 @@
 package wikifier
 
 type tocBlock struct {
+	secCount int
 	*parserBlock
 }
 
 func newTocBlock(name string, b *parserBlock) block {
-	return &tocBlock{b}
+	return &tocBlock{0, b}
 }
 
 func (toc *tocBlock) html(page *Page, el element) {
-	secCount := 0
 	el.setTag("ul")
 	el.addHTML(HTML("<li><strong>Contents</strong></li>"))
 
 	// add each top-level section
 	for _, child := range page.main.blockContent() {
 		if sec, ok := child.(*secBlock); ok {
-			tocAdd(sec, el, page)
-			secCount++
+			toc.tocAdd(sec, el, page)
 		}
 	}
 
 	// don't show the toc if there are <2 on the page
-	if secCount < 2 {
+	if toc.secCount < 2 {
 		el.hide()
 	}
 }
 
-func tocAdd(sec *secBlock, addTo element, page *Page) {
+func (toc *tocBlock) tocAdd(sec *secBlock, addTo element, page *Page) {
+	toc.secCount++
 
 	// create an item for this section
 	var subList element
@@ -47,7 +47,7 @@ func tocAdd(sec *secBlock, addTo element, page *Page) {
 			if subList == nil {
 				subList = addTo.createChild("ul", "")
 			}
-			tocAdd(secChild, subList, page)
+			toc.tocAdd(secChild, subList, page)
 		}
 	}
 }
