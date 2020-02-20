c4bc925c6d3b1186c19f464c14a87ebab46fc7ef
Mitchell Cooper <githubspam@rlygd.net>
2020-02-19 20:44:07 -0800

    in toc{}, hide if less than 2 headings on page

--- wikifier/block-toc.go
+++ wikifier/block-toc.go
@@ -9,10 +9,18 @@ func newTocBlock(name string, b *parserBlock) block {
 }
 
 func (toc *tocBlock) html(page *Page, el element) {
+
+	// don't show the toc if there are <2 on the page
+	blocks := page.main.blockContent()
+	if len(blocks) < 2 {
+		el.hide()
+	}
+
 	el.setTag("ul")
-	el.addHTML(HTML("<strong>Contents</strong>"))
+	el.addHTML(HTML("<li><strong>Contents</strong></li>"))
+
 	// add each top-level section
-	for _, child := range page.main.blockContent() {
+	for _, child := range blocks {
 		if sec, ok := child.(*secBlock); ok {
 			tocAdd(sec, el, page)
 		}
