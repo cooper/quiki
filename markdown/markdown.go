0adda14a32c37812977566c873770c96174d3ea1
Mitchell Cooper <githubspam@rlygd.net>
2020-05-12 15:49:12 -0700

    include goldmark and the guts of a quiki markup renderer based on the html renderer

---
+++ markdown/markdown.go
@@ -0,0 +1,32 @@
+package markdown
+
+import (
+	"bytes"
+	"log"
+
+	"github.com/cooper/quiki/markdown/quikirenderer"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer"
+	"github.com/yuin/goldmark/util"
+)
+
+// Run parses Markdown and renders quiki soure code.
+func Run(input []byte) []byte {
+	md := goldmark.New(
+		goldmark.WithRenderer(renderer.NewRenderer(renderer.WithNodeRenderers(
+			util.Prioritized(quikirenderer.NewRenderer(), 1000),
+		))),
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+	)
+	var buf bytes.Buffer
+	if err := md.Convert(input, &buf); err != nil {
+		log.Println(err)
+		// TODO: proper error handling here
+	}
+	return buf.Bytes()
+}
