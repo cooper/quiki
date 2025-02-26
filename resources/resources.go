d3407e103e16f2fa6b7a814b0e0f7676b1a8b452
Mitchell Cooper <githubspam@rlygd.net>
2025-02-26 11:08:04 -0500

    fix resources #122

--- resources/resources.go
+++ resources/resources.go
@@ -6,11 +6,7 @@ import (
 	"log"
 )
 
-//go:embed adminifier/*
-var adminifierFS embed.FS
-
-//go:embed webserver/*
-var webserverFS embed.FS
+// PUBLIC
 
 // Adminifier provides access to the adminifier resource files.
 var Adminifier fs.FS
@@ -19,19 +15,33 @@ var Adminifier fs.FS
 var Webserver fs.FS
 
 // Wikis provides embedded base wikis.
-//
+var Wikis fs.FS
+
+// PRIVATE
+
+//go:embed adminifier/*
+var adminifierFs embed.FS
+
+//go:embed webserver/*
+var webserverFs embed.FS
+
 //go:embed wikis/*
-var Wikis embed.FS
+var wikisFs embed.FS
 
 func init() {
 	var err error
-	Adminifier, err = fs.Sub(adminifierFS, "adminifier")
-	if err != nil {
-		log.Fatal(err)
+	var resources = map[string]struct {
+		fs       embed.FS
+		accessor *fs.FS
+	}{
+		"adminifier": {adminifierFs, &Adminifier},
+		"webserver":  {webserverFs, &Webserver},
+		"wikis":      {wikisFs, &Wikis},
 	}
-
-	Webserver, err = fs.Sub(webserverFS, "webserver")
-	if err != nil {
-		log.Fatal(err)
+	for name, resource := range resources {
+		*resource.accessor, err = fs.Sub(resource.fs, name)
+		if err != nil {
+			log.Fatalf("failed to access %s resources: %v", name, err)
+		}
 	}
 }
