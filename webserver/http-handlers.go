8c6304035a7d5de6ceb978555e13c084dc610a49
Mitchell Cooper <githubspam@rlygd.net>
2025-08-23 16:10:58 -0400

    log

--- webserver/http-handlers.go
+++ webserver/http-handlers.go
@@ -101,6 +101,7 @@ func handlePage(wi *WikiInfo, relPath string, w http.ResponseWriter, r *http.Req
 // image request
 func handleImage(wi *WikiInfo, relPath string, w http.ResponseWriter, r *http.Request) {
 	log.Printf("DEBUG: handleImage called with relPath: %s", relPath)
+	log.Printf("DEBUG: wi.pregenerateManager is nil: %v", wi.pregenerateManager == nil)
 	result := wi.pregenerateManager.GenerateImageSync(relPath, true)
 	log.Printf("DEBUG: handleImage got result, calling handleResponse")
 	handleResponse(wi, result, w, r)
